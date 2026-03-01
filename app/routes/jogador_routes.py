effade92a313bb2a04e0839ffb8db2f401735338
Luzivam Santos <luzivam.santos28@outlook.com>
2026-03-01 15:13:59 -0300

    modificado  app/routes/jogador_routes.py com a route main, add new file:   app/static/css/index.css para estilizar a página, add new file:   app/templates/bases/bases.html implemetado com font e um link de estilo, add new file:   app/templates/index.html implementado com extends da bases.html add new fuleapp/templates/jogadores.html modified:   app/static/css/index.css modified:   app/templates/bases/bases.html modified:   app/templates/index.html

--- app/routes/jogador_routes.py
+++ app/routes/jogador_routes.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 jogador_bp = Blueprint('jogador', __name__)
 
+@jogador_bp.route("/")
+def index():
+    return render_template("index.html")
+
 @jogador_bp.route("/jogadores")
 def lista_jogadores():
     return render_template("jogadores.html")
